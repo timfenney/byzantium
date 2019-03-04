2e2be6f0d8dff3afb5b600ea841924d046c6cc61
Timothy Fenney <kodekata@gmail.com>
2019-03-04 16:28:26 -0500

    Hacking some on the Keyboard state machine and the server.

--- byzantium/keyboard.py
+++ byzantium/keyboard.py
@@ -10,58 +10,131 @@ from .events import KEYDOWN, KEYHOLD, KEYUP
 from evdev.ecodes import ecodes
 
 # bit posititions for modifiers in HID reports
-LEFT_CONTROL  =   1
-LEFT_SHIFT    =   2
-LEFT_ALT      =   4
-LEFT_META     =   8
-RIGHT_CONTROL =  16
-RIGHT_SHIFT   =  32
-RIGHT_ALT     =  64
-RIGHT_META    = 128
-
-BIT_FOR_EVENT_MODIFIER = {
-}
+# these are valid indexes for storage in a list;
+# the positions are such that 2**CONSTANT will be
+# the value of the flag for the HID report
+# the constants are named as in:
+# https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+KEY_LEFTCTRL   = 0
+KEY_LEFTSHIFT  = 1
+KEY_LEFTALT    = 2
+KEY_LEFTMETA   = 3
+KEY_RIGHTCTRL  = 4
+KEY_RIGHTSHIFT = 5
+KEY_RIGHTALT   = 6
+KEY_RIGHTMETA  = 7
+NUM_MODIFIERS  = 8
 
 MAX_KEYS = 6 # no more than these can be pressed
-NO_KEY = -1 # sentinel value
 
-class KeyBoard(object):
+# This dict allows us to detect modifiers, so they can be handled differently in reports.
+MODIFIERS = {
+    29:  KEY_LEFTCTRL,
+    56:  KEY_LEFTALT,
+    125: KEY_LEFTMETA,
+    42:  KEY_LEFTSHIFT,
+    100: KEY_RIGHTALT,
+    126: KEY_RIGHTMETA,
+    54:  KEY_RIGHTSHIFT,
+    97:  KEY_RIGHTCTRL
+}
+
+class StateMachine(object):
+    '''This is a state machine for producing HID keyboard reports. The protocol has
+       inherent limitations, such as only 6 non-modifier keys may be pressed at a time.
+       Additional keys are thrown away; we could switch to LRU or something if necessary.'''
+    
     def __init__(self):
-        self.modifiers_bitmask = 0
+        '''Set attributes for modelling the state.'''
+        self.modifiers = [False] * NUM_MODIFIERS
         self.non_modifiers = []
-        
-    def keydown(self, key):
-        if not is_modifier(key):
-            current_keys = len(self.non_modifiers)
-            if current_keys < MAX_KEYS:
-                self.non_modifiers.append(key)
-            else:
-                self.non_modifiers[0] = key
+
+    def key_down(self, key):
+        '''Press the key. If it is a mod, set the flag, otherwise add the key if it fits.'''
+        if key in MODIFIERS:
+            index = MODIFIERS[key]
+            self.modifiers[index] = True
         else:
-            if not key in BIT_FOR_EVENT_MODIFIER:
-                print 'arrgghh, key not found: ' + key
-                raise Exception
-            else:
-                bit = BIT_FOR_EVENT_MODIFIER[key]
-                self.modifiers_bitmask |= bit # set bit
-                
-    def keyup(self, key):
-        if not is_modifier(key):
+            if len(self.non_modifiers) < MAX_KEYS:
+                self.non_modifiers.append(key)
+
+    def key_up(self, key):
+        '''Release the key. If it is a mod, clear the flag, otherwise remove the key.'''
+        if key in MODIFIERS:
+            index = MODIFIERS[key]
+            self.modifiers[index] = False
+        else:
+            # This might raise, in the case that we have pressed more keys than MAX_KEYS.
             try:
                 self.non_modifiers.remove(key)
             except:
                 pass
-        else:
-            if not key in BIT_FOR_EVENT_MODIFIER:
-                print 'arrgghh, key not found: ' + key
-                raise Exception
-            else:
-                bit = BIT_FOR_EVENT_MODIFIER[key]
-                mask = ~bit
-                self.modifiers_bitmask &= mask
 
-    # fixme: have to do something like comment below, also see
-    # the url: https://www.rmedgar.com/blog/using-rpi-zero-as-keyboard-send-reports
-    def report(self):
-        pass
-        # return chr(self.modifiers_bitmask) + chr(0) + chr(0)*5
+class ReportFormatter(object):
+    '''This class is responsibile for formatting StateMachine data for HID reports.'''
+    def format_modifiers(self, modifiers):
+        '''Format the modifiers as a bitmask. `modifiers` is an iterable of booleans.'''
+        value = 0
+        for i, flag in enumerate(modifiers):
+            if flag:
+                value += 2**i
+        return value
+
+    def format_non_modifiers(self, non_modifiers):
+        '''Return the non-modifier keys formatted as a string of bytes up to, and padded to MAX_KEYS length.'''
+        value = ''
+        for key in non_modifiers:
+            value += chr(key)
+        padding = MAX_KEYS - len(non_modifiers)
+        if padding > 0:
+            for i in range(padding):
+                value += chr(0)
+        return value
+
+    def format(self, state_machine):
+        '''Return the formatted report.'''
+        return (self.render_modifiers(state_machine.modifiers)
+                + chr(0)
+                + self.render_non_modifiers(state_machine.non_modifiers))
+
+class Reporter(object):
+    '''This is responsible only for writing the report to the relevant device file.
+       The write needs to be flushed immediately. Also, the file may be opened for
+       reading, to detect events from the host, such as LED state changes.'''
+    def __init__(self, device):
+        '''Set attributes for collaborators.'''
+        self._device = device
+
+    def write(self, report):
+        '''Write the report to the device file.'''
+        with open(self._device, 'wb') as f:
+            f.write(report)
+
+class KeyBoard(object):
+    '''This is a keyboard which is controlled programatically, and which writes HID reports.'''
+    def __init__(self, state_machine, formatter, reporter):
+        '''Set attributes for collaborators.'''
+        self._state_machine = state_machine
+        self._formatter = formatter
+        self._reporter = reporter
+        
+    def keydown(self, key):
+        '''Dispatch to the state machine for changes, then report.'''
+        self._state_machine.key_down(key)
+        self.report()
+
+    def keyup(self, key):
+        '''Dispatch to the state machine for changes, then report.'''
+        self._state_machine.key_up(key)
+        self.report()
+
+    def _report(self):
+        '''Dispatch the formatted report.'''
+        report = self._formatter.format(self._state_machine)
+        self._reporter.write(report)
+
+def build_keyboard(device):
+    '''This function will create the object graph, requiring only a device file such as `/dev/input/event3`.'''
+    return Keyboard(state_machine=StateMachine(),
+                    formatter=ReportFormatter(),
+                    reporter=Reporter(device))
---
+++ byzantium/server.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# This file is a placeholder for the server logic
+from .connect import connection
+from .serialize import deserialize
+from .events import KEYDOWN, KEYHOLD, KEYUP
+from .keyboard import build_keyboard
+
+DATA = 'data'
+TYPE = 'type'
+NULL_CHAR = chr(0)
+CODE = 'scancode'
+DEVICE = '/dev/hidg0'
+
+def main():
+    # fixme: redis details leaking into server.py
+    p = connection().pubsub(ignore_subscribe_messages=True)
+    p.subscribe()
+
+    keyboard = build_keyboard(DEVICE)
+
+    for message in p.listen():
+        message_dict = deserialize(message[DATA])
+        key = message_obj[CODE]
+        if message_obj[TYPE] == KEYDOWN:
+            keyboard.keydown(key)
+        elif message_obj[TYPE] == KEYUP:
+            keyboard.keyup(key)
+        elif message_obj[TYPE] == KEYHOLD:
+            print 'holding!'
+
+if __name__ == '__main__':
+    main()
